ac52f25383590653d804ee40acc4bf7589f237b0
Grhan <gurhanceylan@anadolu.edu.tr>
2016-01-06 13:45:42 +0200

    PCF

--- PolyhedralConicFunctions.py
+++ PolyhedralConicFunctions.py
@@ -10,36 +10,66 @@ class PCF:
         self.gamma = 0
         self.ksi = 0
 
+    def setParam(self, A, B, center):
+        dimension = len(A[0])
+        m = len(A)
+        p = len(B)
+        model = Model()
+        gamma = model.addVar(vtype=GRB.CONTINUOUS, lb=1, name='gamma')
+        ksi = model.addVar(vtype=GRB.CONTINUOUS, lb =0, name='ksi')
+        w = range(m)
+        for i in range(dimension):
+            w[i] = model.addVar(vtype=GRB.CONTINUOUS, name='w[%s]' % i)
+
+        model.update()
+        errorA = {}
+
+        for i in range(m):
+            errorA[i] = model.addVar(vtype=GRB.CONTINUOUS, lb=0, name='errorA[%s]' % i)
+            model.update()
+            model.addConstr(quicksum((A[i][j] - A[center][j]) * w[j] for j in range(dimension)) + (ksi * quicksum(math.fabs(A[i][j] - A[center][j]) for j in range(dimension))) - gamma + 1.0 <= errorA[i])
+        for i in range(p):
+            model.addConstr(quicksum((B[i][j] - A[center][j]) * -w[j] for j in range(dimension)) - (ksi * quicksum(math.fabs(B[i][j] - A[center][j]) for j in range(dimension))) + gamma + 1.0 <= 0)
+
+        model.setObjective(quicksum(errorA[i] for i in errorA) / len(errorA), GRB.MINIMIZE)
+        model.optimize()
+        self.gamma = gamma.X
+        self.ksi = ksi.X
+        for i in range(dimension):
+            self.w.append(w[i].X)
+
 
 class PCFC:
     def __init__(self):
         self.pcfs = list()
+        self.dimension = 0
 
-    def fit(self, X, Y):
-        m = len(X)
-        dimension = len(X[0])
+    def fit(self, A, B):
+        self.dimension=len(A[0])
+        while len(A) !=0 :
 
-        while m != 0:
+            center = random.randint(0,len(A))
+            temp = PCF()
+            temp.setParam(A,B,center)
+            self.pcfs.append(temp)
+            A = self.updateSet(A,self.pcfs[-1],center)
+        return self.pcfs
 
-            model = Model()
-            gamma = model.addVar(vtype=GRB.CONTINUOUS, lb=1, name='gamma')
-            ksi = model.addVar(vtype=GRB.CONTINUOUS, lb =0, name='ksi')
+    def delete(self,lst, indices):
+        indices = set(indices)
+        return [lst[i] for i in xrange(len(lst)) if i not in indices]
 
-            w = range(w)
-            for i in range(dimension):
-                w[i] = model.addVar(vtype=GRB.CONTINUOUS, name='w[%s]' % i)
+    def updateSet(self, A, pc,center):
+        deleted = []
+        for i in range(len(A)):
+            f = quicksum((A[i][j] - A[center][j]) * pc.w[j] for j in range(self.dimension)) + (pc.ksi * quicksum(math.fabs(A[i][j] - A[center][j]) for j in range(self.dimension))) - pc.gamma
 
-            r = random.randint(0, m-1)
+            if f.getValue() <= 0.0:
+                deleted.append(i)
 
-            model.update()
-            errorA = {}
+        return self.delete(A,deleted)
 
-            for i in range(m):
-                if Y[i] == 1.0:
-                    errorA[i] = model.addVar(vtype=GRB.CONTINUOUS, lb=0, name='errorA[%s]' % i)
-                    model.update()
-                    m.addConstr(quicksum((X[i][j] - X[r][j]) * w[j] for j in range(m)) + (ksi * quicksum(math.fabs(X[i][j] - X[r][j]) for j in range(m))) - gamma + 1.0 <= errorA[i])
-                else:
-                    m.addConstr(quicksum((X[i][j] - X[r][j]) * -w[j] for j in range(m)) - (ksi * quicksum(math.fabs(X[i][j] - X[r][j]) for j in range(m))) + gamma + 1.0 <= 0)
 
-            model.setObjective(quicksum(errorA))
+
+
+
--- experiment.py
+++ experiment.py
@@ -1,11 +1,12 @@
 import csv
 import RobustLinearProgramming
+import PolyhedralConicFunctions
 import numpy as np
 import matplotlib.pyplot as plt
 dosya = open("iris.csv","rb")
 reader = csv.reader(dosya, quotechar=',')
-
-"""data = []
+"""
+data = []
 X = []
 Y = []
 for row in reader:
@@ -22,14 +23,30 @@ rlp = RobustLinearProgramming.RLP()
 
 parameters = rlp.fit(X,Y,'10FCV')
 
+prediction = rlp.predict(X)
+
 print(parameters)
-print(rlp.predict(X))"""
+print(rlp.predict(X))
+
+print(np.mean(Y == prediction))"""
+
 
 A = [[-2.0, 0.5], [-2.0, -0.5], [-2.0, 2.0], [-2.0, -2.0], [-0.5, 2.0], [-0.5, -2.0], [0.5, 2.0], [0.5, -2.0], [2.0, 0.5], [2.0, -2.0], [2.0, -0.5], [2.0, 2.0], [12.0, 2.0], [12.0, -2.0], [12.0, 0.5], [12.0, -0.5], [13.5, 2.0], [13.5, -2.0], [14.5, 2.0], [14.5, -2.0], [16.0, -0.5], [16.0, 0.5], [16.0, 2.0], [16.0, -2.0]]
 aa = np.array(A)
 B = [[8.0, -6.0], [6.0, -1.0], [20.0, 6.0], [6.0, -6.0], [15.0, 6.0], [20.0, 1.0], [1.0, 6.0], [-1.0, 6.0], [-6.0, 1.0], [20.0, -6.0], [-6.0, -1.0], [-6.0, -6.0], [8.0, -1.0], [13.0, 6.0], [20.0, -1.0], [6.0, 1.0], [15.0, -6.0], [13.0, -6.0], [8.0, 1.0], [-1.0, -6.0], [-6.0, 6.0], [6.0, 6.0], [8.0, 6.0], [1.0, -6.0]]
 bb = np.array(B)
 
+
+deneme = PolyhedralConicFunctions.PCFC()
+
+pcfcs = deneme.fit(A,B)
+
+for i in pcfcs:
+    print(i.gamma,i.ksi)
+    for j in range(len(i.w)):
+        print(i.w[j])
+
 plt.scatter(aa[:, 0], aa[:, 1], c='b', marker='o')
 plt.scatter(bb[:, 0], bb[:, 1], c='r', marker='^')
 plt.show()
+
